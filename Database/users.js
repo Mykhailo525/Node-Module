573f09761f6fc0c3ea1f034c4cdb7cbb3f6d7909
MykhailoTryndiak <mtrindak@gmail.com>
2023-03-06 19:51:48 +0200

    Initial commit

---
+++ Database/users.js
@@ -0,0 +1,10 @@
+const users=[
+    {name: 'Ostap', age: 20},
+    {name: 'Oleg', age: 25},
+    {name: 'Vira', age: 26},
+    {name: 'Andriy', age: 23}
+]
+
+module.exports={
+    users
+}
--- app.js
+++ app.js
@@ -1,11 +1,86 @@
 const fs = require('fs')
 const path = require('path')
 
+const express=require('express')
+const {users}=require('./Database/users')
+
+
+const app=express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 // Винести базу даних в json.file, при створенні записувати туда нових юзерів через fs
 // При створенні валідацію на імія і вік, імя повинно бути більше 2 символів, вік – не менше нуля
 // На гет, пут, деліт юзерів перевірити чи такий юзер є
 
-fs.mkdir(path.join('Database'),(err)=>{
-    if(err) throw new Error(err.message)
+// app.get('/welcome',(req, res)=>{
+//     res.send('WELCOME!!!')
+//     console.log('welcome')
+//     res.end()
+// })
+
+const PORT=5100;
+
+app.listen(PORT,()=>{
+    console.log(`Server started on port ${PORT}`);
+})
+
+app.get('/users',(req, res)=>{
+    res.json(users)
+})
+
+app.get('/users/:userId',(req, res)=>{
+    const{userId}=req.params
+    const user=users[+userId - 1]
+
+    if(+userId<users.length && userId>0){
+        res.json(user)
+    }else{
+        res.json({message:'User doesnt exist'})
+    }
+
+})
+
+
+app.post('/users',(req, res)=>{
+    const body=req.body
+
+    if(body.name.length>2 && body.age>0){
+        users.push(body)
+        res.json({message:'User Created'})
+    }else{
+        res.json({message:'При створенні валідацію на імія і вік, імя повинно бути більше 2 символів, вік – не менше нуля'})
+    }
+})
+
+
+
+app.put('/users/:userId',(req, res)=>{
+    const updatedUser=req.body
+    const{userId}=req.params
+
+    if(+userId<users.length && userId>0){
+        users[+userId]=updatedUser
+        res.json({
+            message:'User updated',
+            data:users[+userId]
+        })
+    }else{
+        res.json({message:'User doesnt exist'})
+    }
+
+})
+
+
+app.delete('/users/:userId',(req, res)=>{
+    const{userId}=req.params
+
+    if(+userId<users.length && userId>0){
+        users.splice(+userId,1)
+        res.json({message:'user deleted'})
+    }else{
+        res.json({message:'User doesnt exist'})
+    }
+
 })
