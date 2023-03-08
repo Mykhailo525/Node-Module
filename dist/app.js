dab299577145b0e962a407b5da3329e4cc3ed2a3
MykhailoTryndiak <mtrindak@gmail.com>
2023-03-08 18:49:07 +0200

    Initial commit

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
@@ -12,5 +10,5 @@ app.use(express_1.default.urlencoded({ extended: true }));
 console.log("hello");
 const PORT = 5100;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
