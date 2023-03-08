6454157d7f2d829801cc398057cf1575bc9c67da
MykhailoTryndiak <mtrindak@gmail.com>
2023-03-08 21:31:25 +0200

    Initial commit

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,88 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose = __importStar(require("mongoose"));
+const User_model_1 = require("./models/User.model");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-console.log("hello");
 const PORT = 5100;
 app.listen(PORT, () => {
+    mongoose.connect("mongodb://127.0.0.1:27017/node-module");
     console.log(`Server started on port ${PORT}`);
 });
+app.get("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield User_model_1.User.find();
+    res.json(users);
+}));
+app.post("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const body = req.body;
+        const user = yield User_model_1.User.create(Object.assign({}, body));
+        res.json({ message: "User created", data: user });
+    }
+    catch (e) {
+        console.log(e.message);
+    }
+}));
+app.get("/users/:userId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.params;
+    const user = yield User_model_1.User.findById({ _id: userId });
+    res.json(user);
+}));
+app.put("/users/:userId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { userId } = req.params;
+        const user = req.body;
+        const updatedUser = yield User_model_1.User.updateOne({ _id: userId }, Object.assign({}, user));
+        res.json({
+            message: "User updated",
+            data: updatedUser,
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+}));
+app.delete("/users/:userId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.params;
+    yield User_model_1.User.deleteOne({ _id: userId });
+    res.json({
+        message: "User deleted",
+    });
+}));
---
+++ src/models/User.model.ts
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+
+import { EGenders } from "../types/user.types";
+
+const userSchema = new Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email is required"],
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    gender: {
+      type: String,
+      enum: EGenders,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+export const User = model("user", userSchema);
